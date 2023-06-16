ee9c1d50876c17306bf7a9e1027a9abeb4b1a0b0
Sujeeth <sujeeth@MacBook-Air.local>
2023-06-16 15:31:43 +0530

    removenoSSR

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,18 +3,16 @@ import React from "react";
 import Link from "next/link";
 function Footer() {
   return (
-    <>
-      <div className="flex justify-center items-center text-sm text-white">
-        <Link
-          target="_blank"
-          href="https://github.com/sujeethcodes"
-          className="text-zinc-200 flex justify-center items-center">
-          Made By <div className="text-lg m-2">🧑‍💻</div>
-          SujeethCodes{" "}
-        </Link>
-        <div className="text-zinc-300 flex justify-center">@ 2023</div>
-      </div>
-    </>
+    <div className="flex justify-center items-center text-sm text-white">
+      <Link
+        target="_blank"
+        href="https://github.com/sujeethcodes"
+        className="text-zinc-200 flex justify-center items-center">
+        <div>Made By</div> <div className="text-lg m-2">🧑‍💻</div>
+        SujeethCodes{" "}
+      </Link>
+      <div className="text-zinc-300 flex justify-center">@ 2023</div>
+    </div>
   );
 }
 
